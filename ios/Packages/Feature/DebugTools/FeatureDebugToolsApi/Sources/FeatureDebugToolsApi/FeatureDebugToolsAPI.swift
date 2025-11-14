14c3dc7148561784e4e31f8844b68077a99c556d
high <celalnsa@gmail.com>
2025-11-14 20:33:11 +0800

    fix

--- ios/Packages/Feature/DebugTools/FeatureDebugToolsApi/Sources/FeatureDebugToolsApi/FeatureDebugToolsAPI.swift
+++ ios/Packages/Feature/DebugTools/FeatureDebugToolsApi/Sources/FeatureDebugToolsApi/FeatureDebugToolsAPI.swift
@@ -1,10 +1,5 @@
 import SwiftUI
 
-/// Debug Tools Feature Builder Protocol
 public protocol FeatureDebugToolsBuildable {
-    /// Create the debug tools main view
     func makeDebugToolsView() -> AnyView
-
-    /// Create the SwiftData chat debug view
-    func makeChatDebugView() -> AnyView
 }
